2b0b9de8013b414f928ad6770935143605a11da5
EmanuelVictorSilva <Emanuelvictorsilva768@gmail.com>
2023-04-03 10:17:57 -0300

    Modificado

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ c = a % 5
 
 if b == 0 and a != 0 and c != 0:
     print('Fizz')
-elif c == 0 and a != 0:
+elif c == 0 and a != 0 and b != 0:
     print('Buzz')
 elif b == 0 and c == 0 or a ==0:
     print('FizzBuzz')
